c179abebbd4737b50ac91b7b70c6fe93f204a14c
nusrat-007 <njahan11335577@gmail.com>
2020-12-01 06:27:25 +0600

    labtask

---
+++ FINAL_LAB_TASK_1.1_DB/View/home.php
@@ -0,0 +1,21 @@
+<?php
+	include('header.php');
+	
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Home Page</title>
+</head>
+<body>
+	<h1>Welcome home, </h1>
+	<a href="../PHP/create.php">Create New User</a> |
+	<a href="profile.php">Profile</a> |
+	<a href="userlist.php">User List</a> |
+	<a href="../PHP/logout.php">logout</a>
+
+
+</body>
+</html>
