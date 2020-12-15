d68be6e10001e9823997acd191a28f6648aad224
nusrat-007 <njahan11335577@gmail.com>
2020-12-16 01:57:34 +0600

    final commit

---
+++ FINAL_LAB_TASK_4.1_AJAX/script.js
@@ -0,0 +1,163 @@
+function formvalidation()
+{
+    let name = checkName();
+    let email = checkEmail();
+    let gender = checkGender();
+    let dob = checkDob();
+    let blood = checkBlood();
+    let degree = checkDegree();
+
+    if(name == true && email == true && gender == true && dob == true && blood == true && degree == true)
+    {
+        let add = addDB();
+
+        alert('Done!');
+        return true;
+    }
+    else
+    {
+        alert('Please.Try again.');
+        return false;
+    }
+
+}
+
+function checkName()
+{
+    let obj = document.getElementById('name').value;
+    if(obj != '')
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+as
+function checkEmail()
+{
+    let email = document.getElementById("email").value;
+
+    if (email == "")
+    {
+        document.getElementById("emailDiv").innerHTML = "*required";
+        return false;
+    }
+
+    var at = email.indexOf("@");
+    var dot = email.lastIndexOf("\.");
+    if (!(email.length > 0 && at > 0 && email[at + 1] !== "." && dot > at + 1 && dot < email.length && email.indexOf(" ") === -1 && email.indexOf("..") === -1))
+    {
+        document.getElementById("emailDiv").innerHTML = "*invalid";
+        return false;
+    }
+    else
+    {
+        document.getElementById("emailDiv").innerHTML = "*Valid";
+        return true; 
+    }
+
+}
+
+
+function checkGender()
+{
+    let obj1 = document.getElementById('gender1').value;
+    let obj2 = document.getElementById('gender2').value;
+    let obj3 = document.getElementById('gender3').value;
+    let gen;
+
+    if (obj1 !='')
+    {
+        gen =obj1;
+    }
+
+    if (obj2 !='')
+    {
+        gen =obj2;
+    }
+
+    if (obj3 !='')
+    {
+        gen =obj3;
+    }
+
+
+    if(gen != '')
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function checkDob()
+{
+    let obj = document.getElementById('dob').value;
+    if(obj != '')
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function checkBlood()
+{
+    let obj = document.getElementById('blood').value;
+    if(obj != '')
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function checkDegree()
+{
+    let obj1 = document.getElementById('deg1').value;
+    let obj2 = document.getElementById('deg2').value;
+    let obj3 = document.getElementById('deg3').value;
+    let deg;
+
+    if (obj1 !='')
+    {
+        deg += obj1;
+    }
+
+    if (obj2 !='')
+    {
+        deg +=obj2;
+    }
+
+    if (obj3 !='')
+    {
+        deg +=obj3;
+    }
+
+    if(deg != '')
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function addDB()
+{
+    var aName = document.getElementById('name').value;
+    var aEmail = document.getElementById('email').value;
+    var aGender = document.getElementById('gender').value;
+    var aDob = document.getElementById('dob').value;
+    var aBlood = document.getElementById('blood').value;
+    var aDeg = document.getElementById('deg').value;
+    var xhttp = new XMLHttpRequest();
+    
+    xhttp.open('POST', 'abcd.php', true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send(aName&aEmail&aGender&aDob&aBlood&aDeg);
+}
